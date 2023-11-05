c56e93bdb0572d70877ebf6033ee05b75b0e475c
denismp <denisputnam@gmail.com>
2023-11-05 11:30:34 -0500

    Initial code.

---
+++ webscraping/webscraping.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+class Webscraping():
+    
+    def __init__(self) -> None:
+        pass
+
+    def ping(self):
+        print("Hello world...")
+
+        
+def main():
+    web_obj = Webscraping()
+    web_obj.ping()
+
+
+if __name__ == "__main__":
+    main()
